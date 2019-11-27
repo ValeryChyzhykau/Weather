30acd34e174acb44aa7d7926ad3db689c34676ae
Valery Chyzhykau <valery.chyzhykau@innowise-group.com>
2019-11-27 19:45:21 +0300

    three Commit

--- Weather/src/app/app.component.ts
+++ Weather/src/app/app.component.ts
@@ -1,38 +1,58 @@
-import { Component } from '@angular/core';
-import{HttpClient} from '@angular/common/http';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  title = 'Weather';
-  lat:Number;
-  lon:Number;
- public getLocation() {
+export class AppComponent {
+  ngOnInit(){}
+  title = "Weather";
+  lat: Number;
+  lon: Number;
+   getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position: Position) => {
-        if (position) {
-          console.log("Latitude: " + position.coords.latitude +
-            "Longitude: " + position.coords.longitude);
-          this.lat = position.coords.latitude;
-          this.lon = position.coords.longitude;
-          console.log(this.lat);
-          console.log(this.lon);
-        }
-      },
-        (error: PositionError) => console.log(error));
+      navigator.geolocation.getCurrentPosition(
+        (position: Position) => {
+          if (position) {
+            console.log(
+              "Latitude: " +
+                position.coords.latitude +
+                "Longitude: " +
+                position.coords.longitude
+            );
+            this.lat = position.coords.latitude;
+            this.lon = position.coords.longitude;
+            console.log(this.lat);
+            console.log(this.lon);
+          }
+        },
+        (error: PositionError) => console.log(error)
+      );
     } else {
       alert("Geolocation is not supported by this browser.");
     }
   }
-  response:any;
-  constructor(private http: HttpClient){}
-  public Weather(){this.http.get(`https://api.weather.yandex.ru/v1/forecast?lat=${this.lat}&lon${this.lon}X-Yandex-API-Key:3281cfff-c8d0-4db1-a821-15582d089011`)
-        .subscribe((response)=>{this.response = response;
-    console.log(this.response.json())})
 
+    response: any;
+  constructor(private http: HttpClient) {}
+   Weather() {
+    this.http
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=ffce339240c0f0ab8f093aa466e3e8d5`
+      )
+      .subscribe(response => {
+        this.response = response;
+        console.log(this.response);
+       
+      });
   }
+  
+ 
+
+
 }
 
-//https://samples.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=ffce339240c0f0ab8f093aa466e3e8d5
+//https://api.samples.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=ffce339240c0f0ab8f093aa466e3e8d5
+//https://api.weather.yandex.ru/v1/forecast?lat=${this.lat}&lon${this.lon}X-Yandex-API-Key:3281cfff-c8d0-4db1-a821-15582d089011
--- Weather/src/app/app.module.ts
+++ Weather/src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LinkComponent } from './link/link.component';
+import { Routes,RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:"link",component:LinkComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
