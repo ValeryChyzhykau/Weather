52ae0fed0821a11677fb7591f14321fbee960b1e
Valery Chyzhykau <valery.chyzhykau@innowise-group.com>
2019-11-27 16:23:02 +0300

    Two commit

--- Weather/src/app/app.component.ts
+++ Weather/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import{HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'Weather';
+  lat:Number;
+  lon:Number;
+ public getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: Position) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude +
+            "Longitude: " + position.coords.longitude);
+          this.lat = position.coords.latitude;
+          this.lon = position.coords.longitude;
+          console.log(this.lat);
+          console.log(this.lon);
+        }
+      },
+        (error: PositionError) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  response:any;
+  constructor(private http: HttpClient){}
+  public Weather(){this.http.get(`https://api.weather.yandex.ru/v1/forecast?lat=${this.lat}&lon${this.lon}X-Yandex-API-Key:3281cfff-c8d0-4db1-a821-15582d089011`)
+        .subscribe((response)=>{this.response = response;
+    console.log(this.response.json())})
+
+  }
 }
+
+//https://samples.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=ffce339240c0f0ab8f093aa466e3e8d5
